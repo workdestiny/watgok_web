4866b52e0cb3b1680bf4eb5062ec8d459d2eab41
workdestiny <workdestiny@hotmail.com>
2018-11-20 21:26:30 +0700

    add session func

--- app/session.go
+++ app/session.go
@@ -1,18 +1,38 @@
 package app
 
 import (
-	"context"
-
+	"github.com/moonrhythm/hime"
 	"github.com/moonrhythm/session"
 )
 
 var (
 	// SessionName is sess
 	SessionName = "sess"
+	// SessionUserKey is key in session
+	SessionUserKey = "userID"
 )
 
-func getSession(ctx context.Context) *session.Session {
+func getSession(ctx *hime.Context) *session.Session {
 	s, err := session.Get(ctx, SessionName)
 	must(err)
 	return s
+}
+
+func setSession(ctx *hime.Context, userID string) error {
+	s, err := session.Get(ctx, SessionName)
+	if err != nil {
+		return err
+	}
+	s.Set(SessionUserKey, userID)
+	return err
+}
+
+func removeSession(ctx *hime.Context) error {
+	s, err := session.Get(ctx, SessionName)
+	if err != nil {
+		return err
+	}
+	s.Del(SessionUserKey)
+	return nil
+
 }
