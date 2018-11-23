2a99624c2fd376da1012146c1673c48ee4e9386c
Nithiszz <undeadkeng@hotmail.com>
2018-11-23 14:36:04 +0700

    update reload laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,6 @@ let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
 mix.setPublicPath('./public');
-mix.sass('resources/assets/sass/app.scss', 'public/css');
+mix.sass('resources/assets/sass/app.scss', 'public/css')
+   .version();
 
