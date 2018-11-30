c66e931fc3655191f44516244f1b01d96884bbb1
workdestiny <workdestiny@hotmail.com>
2018-11-30 15:10:27 +0700

    add repo user

---
+++ repository/queryer.go
@@ -0,0 +1,10 @@
+package repository
+
+import "database/sql"
+
+// Queryer interface
+type Queryer interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
--- repository/user.go
+++ repository/user.go
@@ -1,6 +1,39 @@
 package repository
 
+import "github.com/workdestiny/watgok_web/entity"
+
 //GetUser get user data
-func GetUser() error {
-	return nil
+func GetUser(q Queryer, userID string) (*entity.UserModel, error) {
+	var u entity.UserModel
+	err := q.QueryRow(`
+			SELECT id, name, display, role
+			  FROM users
+			 WHERE id = $1
+	`, userID).Scan(&u.ID, &u.Name, &u.Display, &u.Role)
+
+	return &u, err
+}
+
+//GetUserByFBID get by facebook id
+func GetUserByFBID(q Queryer, fbID string) (*entity.UserModel, error) {
+	var u entity.UserModel
+	err := q.QueryRow(`
+			SELECT id, name, display, role
+			  FROM users
+			 WHERE fb_id = $1
+	`, fbID).Scan(&u.ID, &u.Name, &u.Display, &u.Role)
+
+	return &u, err
+}
+
+//CreateUser create new
+func CreateUser(q Queryer, name, display, fbID string, role entity.Role) (string, error) {
+	var id string
+	err := q.QueryRow(`
+		INSERT INTO users
+			        (name, display, fb_id, role)
+			 VALUES ($1, $2, $3, $4)
+		  RETURNING id;
+	`, name, display, fbID, role).Scan(&id)
+	return id, err
 }
